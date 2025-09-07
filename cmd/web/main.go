c57c62b3544d20cd35daa05b91496b36b81d63ed
PrathameshGandule <prathameshgandule1@gmail.com>
2025-09-07 18:47:46 +0530

    chapter 6 - middlewares

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,7 +21,7 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:#PAkd410199@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "web:dS79d(6[@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
